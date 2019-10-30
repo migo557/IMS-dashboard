45a4216913e61048c40b0d214df4f557a17b0a42
vitalii.toderian <vitalii.toderian@gmail.com>
2019-10-30 18:52:54 +0200

    user model created, modified auth-service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,61 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../models';
+import {CommonHttpService} from "./common-http.service";
 
-import { UserManager, UserManagerSettings, User } from 'oidc-client';
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
 
-@Injectable({
-  providedIn: 'root',
-})
-export class AuthService {
+  private userSource: BehaviorSubject<User>;
+  public user$: Observable<User>;
 
-  private manager = new UserManager(getClientSettings());
-  private user: User = null;
-  constructor() {
-    this.manager.getUser().then(user => {
-      this.user = user;
-    });
+  constructor(private commonHttp: CommonHttpService) {
+    this.userSource = new BehaviorSubject<User>(null);
+    this.user$ = this.userSource.asObservable();
   }
 
-  isLoggedIn(): boolean {
-    return this.user != null && !this.user.expired;
+  public announceUserChange(user: User) {
+    this.userSource.next(user);
   }
-
-  getClaims(): any {
-    return this.user.profile;
-  }
-
-  getAuthorizationHeaderValue(): string {
-    return `${this.user.token_type} ${this.user.access_token}`;
-  }
-
-  startAuthentication(): Promise<void> {
-    return this.manager.signinRedirect();
-  }
-
-  getUserId() {
-    return 1; // this id wil be used as default while developing
-  }
-
-  completeAuthentication(): Promise<void> {
-    return this.manager.signinRedirectCallback().then(user => {
-      window.localStorage.setItem("auth_token", user.id_token);
-      debugger
-      this.user = user;
-    });
-  }
-}
-
-
-export function getClientSettings(): UserManagerSettings {
-  return {
-    metadataUrl: "https://accounts.google.com/.well-known/openid-configuration",
-    authority: 'https://accounts.google.com/o/oauth2/v2/auth',
-    client_id: '329518629784-7kuske73oob72h6p0ihgd442j99qrbmc.apps.googleusercontent.com',
-    redirect_uri: 'http://localhost:4200/auth-callback',
-    post_logout_redirect_uri: 'http://localhost:4200/',
-// tslint:disable-next-line:quotemark
-    response_type: "id_token token",
-    scope: "openid profile https://www.googleapis.com/auth/admin.directory.rolemanagement.readonly https://www.googleapis.com/auth/admin.directory.user.readonly https://www.googleapis.com/auth/admin.directory.customer.readonly",
-    filterProtocolClaims: true,
-    loadUserInfo: true
-  };
+  //todo: Complete authentication service
 }
